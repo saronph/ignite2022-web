776ea14f90d3ab6d4fba596964cf5f160b10edde
Saron Medeiros Philippi <saron.ph@gmail.com>
2022-06-21 20:56:00 -0300

    added post layout

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,6 +35,20 @@ export function Post() {
         <p><a href="">👉 jane.design/doctorcare</a></p>
         <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">
+            Publicar
+          </button>
+        </footer>
+      </form>
     </article>
   );
 }
